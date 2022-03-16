8839f112ebb4930ad7421319400e02aa0c8ad791
GM <gmathur1001gmail.com>
2022-03-16 21:03:15 +0530

    completed

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const args = ["dd-mm-yyyyy"];
-
 class fmDateTime {
+  months() {
+    return [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+  }
+
+  daysOfWeek() {
+    return [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  }
+
   /**
    * @param {Date} date - The date
    * @param {string} format - The string
    */
 
-  getFormattedCurrentDate(date, format) {
-    const DATE = date.getUTCDate();
-    const MONTH = date.getUTCMonth() + 1; // +1 since UTC returns month number 1 less
-    const YEAR = date.getUTCFullYear();
+  formatDate(date, format) {
+    const DATE = date.getDate();
+    const MONTH = date.getMonth();
+    const YEAR = date.getFullYear();
 
     let splitted;
     let seperator;
@@ -23,13 +50,20 @@ class fmDateTime {
       splitted = format.split(" ");
       seperator = " ";
     }
+
     for (var i = 0; i < splitted.length; i++) {
       if (splitted[i].includes("d")) {
         splitted[i] = `${DATE}`;
       }
-      if (splitted[i].includes("m")) {
-        splitted[i] = `${MONTH}`;
+
+      {
+        if (splitted[i] === "month") {
+          splitted[i] = `${this.Months()[MONTH - 1]}`;
+        } else if (splitted[i].includes("m")) {
+          splitted[i] = `${MONTH}`;
+        }
       }
+
       if (splitted[i].includes("y")) {
         splitted[i] = `${YEAR}`;
       }
@@ -37,9 +71,42 @@ class fmDateTime {
     return splitted.join(seperator);
   }
 
-  // returns seconds
+  /**
+   * @param {Date} date - The date
+   */
+
   getSecondsSinceEpoch(date) {
-    const milliseconds = date.getTime();
-    return milliseconds / 1000;
+    const DATE = date.getDate();
+    const MONTH = date.getMonth();
+    const YEAR = date.getFullYear();
+
+    const secondsInDate = DATE * 86400;
+    const secondsInMonth = MONTH * 2678400;
+    const secondsInYear = YEAR * 31536000;
+
+    return secondsInDate + secondsInMonth + secondsInYear;
   }
+  getMillisecondsSinceEpoch(date) {
+    const DATE = date.getDate();
+    const MONTH = date.getMonth();
+    const YEAR = date.getFullYear();
+
+    const milliSecondsInDate = DATE * 86400000;
+    const milliSecondsInMonth = MONTH * 2678400000;
+    const milliSecondsInYear = YEAR * 31536000000;
+
+    return milliSecondsInDate + milliSecondsInMonth + milliSecondsInYear;
+  }
+
+  getMonth(date) {
+    return this.months()[date.getMonth()];
+  }
+  getDay(date) {
+    return this.daysOfWeek()[date.getDay()];
+  }
+  getYear(date) {
+    return date.getFullYear();
+  }
+
 }
+
