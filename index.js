661cfa1c6e6f9b83773e2714d3088d0f221f68b5
GM <gmathur1001gmail.com>
2022-03-16 15:36:09 +0530

    getFormattedCurrentDate function completed

--- index.js
+++ index.js
@@ -1,57 +1,45 @@
-
 const args = ["dd-mm-yyyyy"];
 
-const fmDateTime = () => {
+class fmDateTime {
   /**
    * @param {Date} date - The date
    * @param {string} format - The string
    */
 
-  function getCurrentDate(date , format) {
-    let result;
-    const DATE = date.getUTCDate()
-    const MONTH = date.getUTCMonth() + 1 // +1 since UTC returns month number 1 less
-    const YEAR = date.getUTCFullYear()
+  getFormattedCurrentDate(date, format) {
+    const DATE = date.getUTCDate();
+    const MONTH = date.getUTCMonth() + 1; // +1 since UTC returns month number 1 less
+    const YEAR = date.getUTCFullYear();
 
-
-    // check if format string contains -  
-    if (format.includes('-')) {}
-
-    else if (format.includes('/')) {}
-
-    else if (format.includes(' ')) {}
-
-
-    switch (format) {
-      case "dd-mm-yyyy":
-        result = `${DATE}-${MONTH}-${YEAR}`
-        break;
-      case "dd/mm/yyyy":
-        result = `${DATE}/${MONTH}/${YEAR}`
-        break;
-      case "d-m-yyyy":
-        result = `${DATE}-${MONTH}-${YEAR}`
-        break;
-      case "dd-mm-yyyy":
-        result = `${DATE}-${MONTH}-${YEAR}`
-        break;
-
-      default:
-        break;
+    let splitted;
+    let seperator;
+    if (format.includes("-")) {
+      splitted = format.split("-");
+      seperator = "-";
+    } else if (format.includes("/")) {
+      splitted = format.split("/");
+      seperator = "/";
+    } else if (format.includes(" ")) {
+      splitted = format.split(" ");
+      seperator = " ";
     }
-
-    return result;
+    for (var i = 0; i < splitted.length; i++) {
+      if (splitted[i].includes("d")) {
+        splitted[i] = `${DATE}`;
+      }
+      if (splitted[i].includes("m")) {
+        splitted[i] = `${MONTH}`;
+      }
+      if (splitted[i].includes("y")) {
+        splitted[i] = `${YEAR}`;
+      }
+    }
+    return splitted.join(seperator);
   }
 
-
-  // returns seconds 
-  function getSecondsSinceEpoch(date) {
-      const milliseconds = date.getTime()
-      return milliseconds / 1000  
+  // returns seconds
+  getSecondsSinceEpoch(date) {
+    const milliseconds = date.getTime();
+    return milliseconds / 1000;
   }
-};
-
-var epoch = new Date(2000, 0, 1, 0, 2, 0, 0);
-var cur = new Date()
-
-console.log(cur.getUTCMonth());
+}
