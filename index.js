16563ba9df45761f785e9dadb569270f66b8a3af
GM <gmathur1001gmail.com>
2022-03-16 13:52:08 +0530

    small changess

--- index.js
+++ index.js
@@ -11,13 +11,36 @@ const fmDateTime = () => {
     let result;
     const DATE = date.getUTCDate()
     const MONTH = date.getUTCMonth() + 1 // +1 since UTC returns month number 1 less
+    const YEAR = date.getUTCFullYear()
+
+
+    // check if format string contains -  
+    if (format.includes('-')) {}
+
+    else if (format.includes('/')) {}
+
+    else if (format.includes(' ')) {}
+
+
     switch (format) {
       case "dd-mm-yyyy":
+        result = `${DATE}-${MONTH}-${YEAR}`
+        break;
+      case "dd/mm/yyyy":
+        result = `${DATE}/${MONTH}/${YEAR}`
+        break;
+      case "d-m-yyyy":
+        result = `${DATE}-${MONTH}-${YEAR}`
+        break;
+      case "dd-mm-yyyy":
+        result = `${DATE}-${MONTH}-${YEAR}`
         break;
 
       default:
         break;
     }
+
+    return result;
   }
 
 
